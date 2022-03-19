e3766fcc00b9d0c9c149ca22fbcb63b3db0a0dd1
eddy08 <bhardwajharsh08@gmail.com>
2022-03-19 22:37:18 +0530

    Intial Commit 🧡

--- src/app/books/actions/books-page.actions.ts
+++ src/app/books/actions/books-page.actions.ts
@@ -1,2 +1,23 @@
 import { createAction, props } from "@ngrx/store";
 import { BookRequiredProps } from "src/app/shared/models";
+
+//[Book Page] Create a Book
+// - BookRequiredProps
+const createBook = createAction("[Book Page] Create a Book",
+props<{book:BookRequiredProps}>()
+);
+//[Book Page] Update a Book
+// - BookRequiredProps
+// - ID
+const updateBook = createAction("[Book Page] Update a Book",
+props<{book:BookRequiredProps,ID:string}>()
+)
+
+//[Book Page] Delete a Book
+// - ID
+const deleteBook = createAction("[Book Page] Delete a Book",
+props<{ID:string}>()
+)
+//[Book Page] Cancel Editing a Book
+// - ID
+const editBook = createAction("[Book Page] Cancel Editing a Book")
